0340ebdfa445e11740487e22ed941c30ebb8eaaa
Ricardo <ricardomartims.11@gmail.com>
2022-01-17 13:37:10 +0000

    17jan

--- app/src/main/java/com/example/washeasier/MapsActivity.java
+++ app/src/main/java/com/example/washeasier/MapsActivity.java
@@ -1,9 +1,18 @@
 package com.example.washeasier;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,13 +25,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -43,10 +55,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
         LatLng elefanteAzul = new LatLng(37.75104229604585, -25.65657357685678);
         mMap.addMarker(new MarkerOptions().position(elefanteAzul).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(elefanteAzul,15f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(elefanteAzul, 15f));
 
 
     }
