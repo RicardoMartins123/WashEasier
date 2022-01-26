414f54e5babaa6b730621d221a4ae4c5a3f5183c
Ricardo <ricardomartims.11@gmail.com>
2022-01-26 15:55:21 +0000

    aula24jan

--- app/src/main/java/com/example/washeasier/MainActivity.java
+++ app/src/main/java/com/example/washeasier/MainActivity.java
@@ -1,26 +1,36 @@
 package com.example.washeasier;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnMapa;
-    RadioGroup radioGroupRaio;
-    RadioButton radioButton1km;
-    RadioButton radioButton5km;
-    RadioButton radioButton10km;
-    RadioButton radioButton15km;
-    RadioButton radioButton20km;
-    RadioButton radioButtonIlimitado;
+    private Button btnMapa;
+    private RadioGroup radioGroupRaio;
+    private RadioButton radioButton1km;
+    private RadioButton radioButton5km;
+    private RadioButton radioButton10km;
+    private RadioButton radioButton15km;
+    private RadioButton radioButton20km;
+    private RadioButton radioButtonIlimitado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +46,46 @@ public class MainActivity extends AppCompatActivity {
         radioButton20km=findViewById(R.id.rb20km);
         radioButtonIlimitado=findViewById(R.id.rbIlimitado);
 
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }else{
-            // Write you code here if permission already given.
-            btnMapa.setOnClickListener(v ->{
-                Intent intent= new Intent(this, MapsActivity.class);
-                startActivity(intent);
-            });
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            finish();
+            return;
         }
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withContext(MainActivity.this)
+                        .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                                if(permissionDeniedResponse.isPermanentlyDenied()){
+                                    AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                                    builder.setTitle("Permission Denied")
+                                            .setMessage("Permission to acess device location is permanently denied. You need to go to settings to allow the permission.")
+                                            .setNegativeButton("Cancel",null)
+                                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                                }
+                                            });
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.continuePermissionRequest();
+                            }
+                        })
+                        .check();
+            }
+        });
     }
 
 }
